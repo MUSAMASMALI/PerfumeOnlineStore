48eb13f54171c2fc5e460f91657071c62483aec0
MUSAMASMALI <musa14077@yahoo.com>
2022-01-24 10:09:56 +0300

    final project

--- PerfumeOnlineStore 2/PerfumeOnlineStore/Controller/DetailsViewController.swift
+++ PerfumeOnlineStore 2/PerfumeOnlineStore/Controller/DetailsViewController.swift
@@ -4,15 +4,19 @@ import Firebase
 class DetailsViewController: UIViewController {
     var selectedPost:Post?
     var selectedPostImage:UIImage?
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var descriptionLable: UILabel!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var postTitleLabel: UILabel!
     @IBOutlet weak var numberLable: UILabel!
     @IBOutlet weak var postDescriptionLabel: UITextView!
+    
     @IBOutlet weak var mobileNumber: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        numberLable.text = "number".localized
+        descriptionLable.text = "description".localized
+        numberLable.text = "number1".localized
+        titleLable.text = "title".localized
         navigationItem.backBarButtonItem = UIBarButtonItem(
             title: "backButton".localized, style: .plain, target: nil, action: nil)
         if let selectedPost = selectedPost,
